2f4c3e885780d025affc715c7276631e08b12e7d
forestsoob <vini20022203@gmail.com>
2024-11-08 03:20:21 +0900

    feat: 사용자 앱 초기 설정 및 모델, 뷰 생성

---
+++ project/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    nickname = models.CharField(max_length=8, unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nickname
