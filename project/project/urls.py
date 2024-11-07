c5dd3805613f089b6fe10fd84f7d001bfc5e312a
forestsoob <vini20022203@gmail.com>
2024-11-08 03:44:56 +0900

    feat: project  URL 설정

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import UserViewSet, GroupViewSet
-
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/users/', include('users.urls')),  # users 앱 URL 포함
+    path('api/groups/', include('groups.urls')),  # groups 앱 URL 포함
 ]
