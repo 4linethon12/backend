374f4428a9109fb0d4786cc27888f716231bfae8
Song Jimin <songwlals050328@gmail.com>
2024-11-08 22:57:46 +0900

    feature: views.py에 회원가입 추가

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import User
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer, TokenRefreshSerializer
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer, TokenRefreshSerializer, RegisterSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import (
@@ -14,8 +15,16 @@ class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-class TokenObtainPairViewCustom(TokenObtainPairView):
+class LoginView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=201)
+        return Response(serializer.errors, status=400)
 
 class TokenRefreshViewCustom(TokenRefreshView):
     serializer_class = TokenRefreshSerializer
