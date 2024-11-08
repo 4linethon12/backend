057643ef09245e728d8f822af8a8b8bfe9a539c1
forestsoob <vini20022203@gmail.com>
2024-11-09 04:11:31 +0900

    fix: urls.py 수정

--- project/manito/urls.py
+++ project/manito/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import ManitoMessageViewSet
 
 router = DefaultRouter()
-router.register(r'', ManitoMessageViewSet)
+router.register(r'messages', ManitoMessageViewSet)  # 명확한 URL 패턴 지정
 
 urlpatterns = router.urls
