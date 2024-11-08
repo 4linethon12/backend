be8043665178c795a33c8925c9a6a64424240592
Song Jimin <songwlals050328@gmail.com>
2024-11-08 21:56:11 +0900

    feature: views.py에 토큰 관련 로직 연결

--- project/users/views.py
+++ project/users/views.py
@@ -1,7 +1,21 @@
 from rest_framework import viewsets
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer, TokenRefreshSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class TokenObtainPairViewCustom(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class TokenRefreshViewCustom(TokenRefreshView):
+    serializer_class = TokenRefreshSerializer
