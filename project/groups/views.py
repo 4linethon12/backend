e3c1cce88fa9d439e6098a72e9eaa8d0b4301de2
forestsoob <vini20022203@gmail.com>
2024-11-09 03:56:04 +0900

    fix: 미션 랜덤 쿼리 추가

--- project/groups/views.py
+++ project/groups/views.py
@@ -1,7 +1,15 @@
-from rest_framework import viewsets
-from .models import Group
-from .serializers import GroupSerializer
+from rest_framework import viewsets, mixins
+from .models import Group, RecommendedMission
+from .serializers import GroupSerializer, RecommendedMissionSerializer
+import random
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class RecommendedMissionViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = RecommendedMissionSerializer
+
+    def get_queryset(self):
+        missions = list(RecommendedMission.objects.all())
+        return random.sample(missions, 5) 
