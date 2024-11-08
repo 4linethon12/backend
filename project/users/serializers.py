2a21ccb5af63f5dda51f413094ca09701c561856
Song Jimin <songwlals050328@gmail.com>
2024-11-08 21:54:15 +0900

    feature: serializers.py에 토큰 관련 직렬화 추가

--- project/users/serializers.py
+++ project/users/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'nickname', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['nickname'] = user.nickname
+        return token
+
+class TokenRefreshSerializer(TokenRefreshSerializer):
+    pass
