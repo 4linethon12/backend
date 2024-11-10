20f1532d1fda3b54138983ebe4364ca2e2569260
forestsoob <vini20022203@gmail.com>
2024-11-11 03:41:20 +0900

    fix: join url 설정

--- project/groups/urls.py
+++ project/groups/urls.py
@@ -5,8 +5,8 @@ from manito.views import CreateManitoMatchView
 from .views import GroupViewSet, RecommendedMissionViewSet
 
 router = DefaultRouter()
-router.register(r'groups', GroupViewSet)  # GroupViewSet의 엔드포인트
-router.register(r'recommended-missions', RecommendedMissionViewSet, basename='recommended-missions')  # 추천 미션 엔드포인트
+router.register(r'groups', GroupViewSet)
+router.register(r'recommended-missions', RecommendedMissionViewSet, basename='recommended-missions')
 
 urlpatterns = [
     path('<int:group_id>/create-matches/', CreateManitoMatchView.as_view(), name='create-manito-matches'),
