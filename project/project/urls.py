d45e089541afd44f064c3fa5cb62169cc705983a
Song Jimin <songwlals050328@gmail.com>
2024-11-10 20:24:01 +0900

    fix: swagger 오류 수정

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,9 +20,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),  
-    path('api/groups/', include('groups.urls')),  
-    path('api/messages/', include('manito.urls')),
+    path('api/users/', include('users.urls'), name='users'),
+    path('api/groups/', include('groups.urls'), name='groups'),
+    path('api/messages/', include('manito.urls'), name='manito'),
+    path('api-auth/', include('rest_framework.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
