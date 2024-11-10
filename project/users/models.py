eeea8a4aa2386fbd5670d384721c0c489dce9ae0
Song Jimin <songwlals050328@gmail.com>
2024-11-10 23:11:45 +0900

    feature: 유저 커스텀 매니저 설정

--- project/users/models.py
+++ project/users/models.py
@@ -1,6 +1,39 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, nickname, password=None, **extra_fields):
+        if not nickname:
+            raise ValueError('닉네임 필드 비어 있음')
+
+        user = self.model(
+            nickname=nickname,
+            username=nickname,
+            **extra_fields
+        )
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(nickname, password, **extra_fields)
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=8, unique=True)
@@ -17,20 +50,17 @@ class User(AbstractUser):
     USERNAME_FIELD = 'nickname'
     REQUIRED_FIELDS = []
 
+    objects = CustomUserManager()
+
     class Meta:
         db_table = 'user'
 
     def save(self, *args, **kwargs):
         if not self.username:
             self.username = self.nickname
+        if self.password and not self.password.startswith(('pbkdf2_sha256$', 'bcrypt$', 'argon2')):
+            self.password = make_password(self.password)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nickname
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self.save()
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
