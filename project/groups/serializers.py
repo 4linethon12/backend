b12aa4e16e823c67240eac2a5f7cf53e27ec5550
forestsoob <vini20022203@gmail.com>
2024-11-11 03:32:54 +0900

    fix: 그룹 사용자 정보 serializer 추가

--- project/groups/serializers.py
+++ project/groups/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Group, RecommendedMission
+from .models import Group, RecommendedMission, GroupParticipant
 from users.serializers import UserSerializer  
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -14,3 +14,10 @@ class RecommendedMissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecommendedMission
         fields = ['id', 'text']
+
+class GroupParticipantSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = GroupParticipant
+        fields = ['id', 'user', 'joined_at']
