11275f5e77241cdd0427411e0f7d4a1a452dccae
forestsoob <vini20022203@gmail.com>
2024-11-13 00:48:01 +0900

    fix: 사용자 그룹 리스트 추가

--- project/groups/urls.py
+++ project/groups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from manito.views import CreateManitoMatchView
-from .views import GroupViewSet, RecommendedMissionViewSet
+from .views import GroupViewSet, RecommendedMissionViewSet, GroupJoinView
 
 router = DefaultRouter()
 router.register(r'groups', GroupViewSet)
@@ -10,4 +10,5 @@ router.register(r'recommended-missions', RecommendedMissionViewSet, basename='re
 
 urlpatterns = [
     path('<int:group_id>/create-matches/', CreateManitoMatchView.as_view(), name='create-manito-matches'),
+    path('groups/<str:code>/join/', GroupJoinView.as_view(), name='group-join'),
 ] + router.urls
